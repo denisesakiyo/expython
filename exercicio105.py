18dac371fc03cb50850a30f9079f716fa7b43549
Denise <de_20ide@hotmail.com>
2022-07-30 17:52:04 -0300

    exercício 105

--- exercicio105.py
+++ exercicio105.py
@@ -1,14 +1,23 @@
 def notas(*dic, sit = False):
-    
+    x={}
+    x['Tamanho'] = len(dic)
+    x['Maior Nota'] = max(dic)
+    x['Menor Nota'] = min(dic)
+    x['Média'] = sum(dic)/len(dic)
 
-
-
-
+    if sit:
+        if x['Média'] >= 7:
+            x['Situação'] = 'BOA'
+        elif x ['Média']>=5:
+            x['Situação'] = 'Razoável'
+        else:
+            x['Situação'] = 'Ruim'
+    return x
 
 
 
 
 
 #programa principal
-resp = notas(2.5,3.0,6.0,sit = True)
+resp = notas(3.5,2.0,6.0,4,sit = True)
 print(resp)
